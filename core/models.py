1d4c9f53a428c5a83a3e694ab10b0ef2c0b16c84
RichardRojas13 <ri.rojas@duocuc.cl>
2021-07-10 17:32:28 -0400

    first commit

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+#CATEGORIA
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name="Id Categoria")
+    nombreCategoria = models.CharField(max_length=50, verbose_name="Nombre Categoria")
+
+    def _str_(self):
+        return self.nombreCategoria
+
