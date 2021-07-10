0d0c4f5feef87353431960cdf160d18b3074d6c5
RichardRojas13 <ri.rojas@duocuc.cl>
2021-07-10 18:06:15 -0400

    1

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Producto, Venta, Cliente, Usuario, Suscrito
+from .models import Producto, Venta,
 
 # Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Usuario)
 admin.site.register(Producto)
 admin.site.register(Venta)
-admin.site.register(Suscrito)
