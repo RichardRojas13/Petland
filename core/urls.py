7350aee415753771c9cc503ed3fdd8ca451d88df
RichardRojas13 <ri.rojas@duocuc.cl>
2021-07-12 20:18:42 -0400

    1

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
-from .views import index, LoUsRe, registroUser, verificacionUser, donarSuscripcion, cerrarSesion
+from .views import index, LoUsRe, registroUser, verificacionUser, doSuscripcion, cerrarSesion, suscripcionUsuario, desSuscripcion
 
 urlpatterns = [
     path('', LoUsRe, name="LoUsRe.html"),
     path('verificacionUser', verificacionUser, name="verificacionUser"),
     path('registroUser',registroUser, name="registroUser"),
     path('index',index, name="index.html"),
-    path('donarSuscripcion', donarSuscripcion, name="donarSuscripcion.html"),
-    path('cerrarSesion', cerrarSesion, name="cerrarSesion"),
+    path('index/doSuscripcion/', doSuscripcion, name="donarSuscripcion.html"),
+    path('suscripcionUsuario', suscripcionUsuario, name="suscripcionUsuario"),
+    path('desSuscripcion', desSuscripcion, name="desSuscripcion"),
+    path('cerrarSesión', cerrarSesion, name="cerrarSesión"),
 ]
