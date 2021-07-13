eecfda1ee264f27e19aabb04752e1adf07fb6b95
RichardRojas13 <ri.rojas@duocuc.cl>
2021-07-13 13:17:56 -0400

    21

---
+++ core/static/core/fuction/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(document).ready(function() {
+        $(function () {
+            $(window).on('scroll', function () {
+                if ( $(window).scrollTop() > 10 ) {
+                    $('.navbar').addClass('active');
+                } else {
+                    $('.navbar').removeClass('active');
+                }
+            });
+        });
+    });
+});
